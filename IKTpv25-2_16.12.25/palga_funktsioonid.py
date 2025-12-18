df63261d914bbcc2c64dfdf2251237b304eec436
sabirlomonosov-jpg <sabir.lomonosov@gmail.com>
2025-12-18 15:20:01 +0200

    palga_funktsioonid.py

--- IKTpv25-2_16.12.25/palga_funktsioonid.py
+++ IKTpv25-2_16.12.25/palga_funktsioonid.py
@@ -1,6 +1,5 @@
-import abc
-from random import *
-from string import *
+inimesed=["A","B","C","D","E","F"]
+palgad=[1000,1200,2500,800,750,1200]
 
 def lisa_andmeid(inimesed, palgad):
     n = int(input("Mitu inimest lisada: "))
@@ -18,3 +17,46 @@ def kustuta_andmeid(inimesed, palgad):
             palgad.pop(i)
 
 
+def suurim_palk(nimed, palgad):
+    max_palk = max(palgad)
+    print("Suurim palk: ", max_palk)
+    print("Saajad:")
+    for i in range(len(palgad)):
+        if palgad[i] == max_palk:
+            print(nimed[i])
+
+def vaikseim_palk(nimed, palgad):
+    min_palk = min(palgad)
+    print("Vaiksem palk: ", min_palk)
+    print("Saajad:")
+    for i in range(len(palgad)):
+        if palgad[i] == min_palk:
+            print(nimed[i])
+
+def jarjesta_palgad(nimed, palgad, kasvav=True):
+    for i in range(len(palgad)):
+        for j in range(i + 1, len(palgad)):
+            if kasvav:
+                if palgad[i] > palgad[j]:
+                    palgad[i], palgad[j] = palgad[j], palgad[i]
+                    nimed[i], nimed[j] = nimed[j], nimed[i]
+            else:
+                if palgad[i] < palgad[j]:
+                    palgad[i], palgad[j] = palgad[j], palgad[i]
+                    nimed[i], nimed[j] = nimed[j], nimed[i]
+    for i in range(len(nimed)):
+        print(nimed[i], ":", palgad[i])
+
+def vordsed_palgad(nimed, palgad):
+    kontrol = set()
+    leitud = False
+    for palk in palgad:
+        if palgad.count(palk) > 1 and palk not in kontrol:
+            kontrol.add(palk)
+            print(f"Palk {palk} saavad:")
+            for i in range(len(palgad)):
+                if palgad[i] == palk:
+                    print(nimed[i])
+            leitud = True
+    if not leitud:
+        print("Võrdseid palku ei leitud.")
